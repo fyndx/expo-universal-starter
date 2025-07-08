fcc5f4dbd0e0441b980e5f05eb9452135923592c
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-07-08 12:33:00 +0530

    feat: update project structure and improve layout; add platform-specific directories and enhance HomeScreen functionality

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,6 +8,7 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
@@ -21,12 +22,14 @@ export default function RootLayout() {
 	}
 
 	return (
-		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<Stack>
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen name="+not-found" />
-			</Stack>
-			<StatusBar style="auto" />
-		</ThemeProvider>
+		<SafeAreaView style={{ flex: 1 }}>
+			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+				<Stack>
+					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+					<Stack.Screen name="+not-found" />
+				</Stack>
+				<StatusBar style="auto" />
+			</ThemeProvider>
+		</SafeAreaView>
 	);
 }
