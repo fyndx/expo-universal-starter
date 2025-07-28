f28a82a2d0f363f6e496f3c4b08f4b5b3ef01bc4
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-07-28 18:21:33 +0530

    fix: display loading screen during authentication pending state in ProtectedLayout

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,12 +1,13 @@
 import { Redirect, Slot } from "expo-router";
 import { Header } from "~/components/layouts/default/Header";
+import { LoadingScreen } from "~/containers/loading-screen";
 import { authClient } from "~/lib/auth-client";
 
 export default function ProtectedLayout() {
 	const { isPending, data } = authClient.useSession();
 	if (isPending) {
 		// Wait for the authentication state to resolve
-		return null;
+		return <LoadingScreen />;
 	}
 
 	if (!data) {
