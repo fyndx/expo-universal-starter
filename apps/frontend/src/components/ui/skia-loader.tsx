15a9a5ee370a4b6b2f95e44199d067c9f8200c37
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-08-12 21:34:27 +0530

    refactor: separate SkiaLoader implementation for web and remove web-specific logic from main component

--- apps/frontend/src/components/ui/skia-loader.tsx
+++ apps/frontend/src/components/ui/skia-loader.tsx
@@ -1,16 +1,8 @@
-import { WithSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
-
 interface SkiaCanvasProps {
 	component: () => Promise<{ default: React.ComponentType }>;
 }
 
+// noop: This file is intentionally left empty to avoid importing SkiaLoader in the web version.
 export function SkiaLoader({ component }: SkiaCanvasProps) {
-	return (
-		<WithSkiaWeb
-			opts={{
-				locateFile: (file) => `/canvaskit.wasm`,
-			}}
-			getComponent={component}
-		/>
-	);
+	return <></>;
 }
